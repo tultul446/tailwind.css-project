9d9af816c7338fe3c16b3d87b133c9b591c2b419
tultul446 <toltolakther@hotmail.com>
2025-01-17 04:24:41 +0600

    update

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        news: resolve(__dirname, "news.html"),
+       
       },
     },
   },
